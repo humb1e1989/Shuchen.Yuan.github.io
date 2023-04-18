ef82ee60980612222e0c198bd5edde45683ff0ea
lostboi <100217991+humb1e1989@users.noreply.github.com>
2023-04-18 17:04:33 +0800

    Update the 电子木鱼
    css 控制页面排版 电子木鱼直接在网页上运行

--- Electronic Mokugyo.js
+++ Electronic Mokugyo.js
@@ -1,71 +1,71 @@
-var mubang = document.querySelector("#stick");
-mubang.addEventListener("click", clickstick, false);
+document.addEventListener('DOMContentLoaded', () => {
+    var stick = document.querySelector("#stick");
+    var muyu = document.querySelector("#mokugyo");
+    var music = document.querySelector("#music");
+    var auto = document.querySelector("#auto");
 
-var muyu = document.querySelector("#mokugyo");
-muyu.addEventListener("click", clickmokugyo, false);
+    // automatically play the music
+    var autoPlayMusic;
 
-var music = document.querySelector("#music");
-var auto = document.querySelector("#auto");
-
-// 鑷姩鎾斁闊充箰
-var autoPlayMusic;
-// 鏀瑰彉checkbox鏃惰Е鍙�
-auto.addEventListener("change", () => {
-    if (auto.checked) {
-        autoPlay();
-        addNum();
-        setTimeout(() => {
-            playMusic();
-            autoPlayMusic = setInterval(playMusic, 1000);
-            autoAddNum = setInterval(addNum, 1000);
-        }, 500);
-    } else {
-        clearInterval(autoPlayMusic);
-        clearInterval(autoAddNum);
+    autoPlay();
+    addNum();
+    setTimeout(() => {
+        playMusic();
+        autoPlayMusic = setInterval(playMusic, 1000);
+        autoAddNum = setInterval(addNum, 1000);
+    }, 500);
+    // triggered when the check box is changed
+    auto.addEventListener("change", () => {
+        if (auto.checked) {
+            autoPlay();
+            addNum();
+            setTimeout(() => {
+                playMusic();
+                autoPlayMusic = setInterval(playMusic, 1000);
+                autoAddNum = setInterval(addNum, 1000);
+            }, 500);
+        } else {
+            clearInterval(autoPlayMusic);
+            clearInterval(autoAddNum);
+            document.querySelector(".stick").className = "stick";
+        }
+    });
+    // click the stick
+    function clickstick() {
         document.querySelector(".stick").className = "stick";
-    }
-});
-
-// 鐐瑰嚮鏈ㄦ
-function clickstick() {
-    document.querySelector(".stick").className = "stick";
-    window.requestAnimationFrame(function (time) {
+        window.requestAnimationFrame(function (time) {
         window.requestAnimationFrame(function (time) {
             document.querySelector(".stick").className = "stick move";
         });
     });
     setTimeout(playMusic, 500);
     setTimeout(addNum, 500);
-}
-// 鎾斁闊充箰
-function playMusic() {
+    }
+    // play the music
+    function playMusic() {
     music.play();
-}
-
-// 鑷姩鏁叉墦鏈ㄩ奔
-function autoPlay() {
+    }
+    // automatically count the number of the clicking
+    function autoPlay() {
     document.querySelector(".stick").className = "stick";
     window.requestAnimationFrame(function (time) {
         window.requestAnimationFrame(function (time) {
             document.querySelector(".stick").className = "stick auto_move";
         });
     });
-}
-
-// 鐐瑰嚮鏈ㄩ奔
-function clickMuyu() {
+    }
+    // clicking the Mokugyo
+    function clickMokugyo() {
     playMusic();
     addNum();
-}
-
-// 浠婃棩鍔熷痉,鎬诲姛寰�
-var todayNum = 0,
-    allNum = 0;
-var today = document.querySelector("#today_count");
-var all = document.querySelector("#all_count");
-// 鍒濆鍖栬幏鍙栨祻瑙堝櫒淇濆瓨鐨勫姛寰�
-getNum();
-function getNum() {
+    }
+    // Daily virtue, total virtue
+    var todayNum = 0,
+        allNum = 0;
+    var today = document.querySelector("#today_count");
+    var all = document.querySelector("#all_count");
+    getNum();
+    function getNum() {
     if (localStorage.key("todayNum")) {
         todayNum = +localStorage.getItem("todayNum");
         today.innerHTML = todayNum;
@@ -78,32 +78,31 @@ function getNum() {
     } else {
         localStorage.setItem("allNum", 0);
     }
-}
-// 鎬诲姛寰�
-
-// 鍔熷痉+1
-
-function addNum() {
+    }
+    // virtue ++
+    function addNum() {
     showtips();
     setTimeout(hiddenTips, 600);
-    // 浠婃棩鍔熷痉+1
+    // daily virtue + 1
     todayNum = +todayNum + 1;
     today.innerHTML = todayNum;
     localStorage.setItem("todayNum", todayNum);
-    // 鎬诲姛寰�+1
+    // total virtue + 1
     allNum = +allNum + 1;
     all.innerHTML = allNum;
     localStorage.setItem("allNum", allNum);
-}
-// 鎺у埗鏄剧ず鍔熷痉+1鐨勫姩鐢�
-var tips = document.querySelector("#tips");
-function showtips() {
+    }
+    // the controller of the addtion for the virtue
+    var tips = document.querySelector("#tips");
+    function showtips() {
     tips.style.visibility = "visible";
     tips.style.opacity = "1";
     tips.style.top = "50px";
-}
-function hiddenTips() {
+    }
+
+    function hiddenTips() {
     tips.style.visibility = "hidden";
     tips.style.opacity = "0";
     tips.style.top = "100px";
 }
+
